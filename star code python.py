f0941531439db2b9f9cf1dca77c4ca9fe432620a
D-Ankush <142439222+D-Ankush@users.noreply.github.com>
2023-08-20 15:34:44 +0530

    Add files via upload

---
+++ star code python.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 14:52:52 2023
+
+@author: Ankush
+"""
+
+import turtle
+t=turtle.Turtle()
+t.left(60)
+t.forward(200)
+t.right(120)
+t.forward(200)
+t.right(150)
+t.forward(250)
+t.right(150)
+t.forward(250)
+t.right(152)
+t.forward(265)
