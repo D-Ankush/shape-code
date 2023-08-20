64180af7bf3389386469efd75777de1de320c669
D-Ankush <142439222+D-Ankush@users.noreply.github.com>
2023-08-20 15:33:51 +0530

    Add files via upload

---
+++ spyder code learning 2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 12:39:25 2023
+
+@author: Ankush
+"""
+
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
