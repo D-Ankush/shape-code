34c5be0945687c6bd9b795188018b8bffab34c6e
D-Ankush <142439222+D-Ankush@users.noreply.github.com>
2023-08-20 15:31:07 +0530

    Add files via upload

---
+++ nested square python code.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import turtle
+t=turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(200)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
