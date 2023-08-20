04d88434b8fd192c0bc9af3b2cb28788ce90aa65
D-Ankush <142439222+D-Ankush@users.noreply.github.com>
2023-08-20 14:31:00 +0530

    Add files via upload

---
+++ cone code python.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 15:46:31 2023
+
+@author: Ankush
+"""
+import turtle
+t=turtle.Turtle()
+t.right(180)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(60)
+t.circle(58, 120)
---
+++ right angle coding 2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 17 12:49:13 2023
+
+@author: Ankush
+"""
+import turtle
+t=turtle.Turtle()
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(135)
+t.forward(282.8)
